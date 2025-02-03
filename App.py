23a7395228e798f4170eeee46b057e24e04f57fe
Tanjil Mahmud Emtu <142157754+TM-EMTU@users.noreply.github.com>
2025-02-03 18:02:57 +0600

    Update App.py

--- App.py
+++ App.py
@@ -69,7 +69,7 @@ with st.sidebar:
 # Initialize the chat engine
 llm_engine = ChatOllama(
     model=selected_model,
-    base_url = "103.47.102.41",  # Fixed missing quote
+    base_url = "localhost:11434",  # Fixed missing quote
     temperature=0.3
 )
 
