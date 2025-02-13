4c9fa20594f67021e8cf62b1d4a04edae9e1171a
TANJIL MAHMUD EMTU <142157754+TM-EMTU@users.noreply.github.com>
2025-02-13 18:21:09 +0600

    Update App.py

--- App.py
+++ App.py
@@ -69,7 +69,7 @@ with st.sidebar:
 # Initialize the chat engine
 llm_engine = ChatOllama(
     model=selected_model,
-    base_url="http://localhost:11434/",  # Fixed the typo
+    base_url="https://ollama-railway-production-81d8.up.railway.app",  # Fixed the typo
     temperature=0.3
 )
 
